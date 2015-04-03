8d4ab1f595f08f3222a0b685bafcec6aa4ebb855
zimbatm <zimbatm@zimbatm.com>
2015-04-03 19:40:04 +0100

    ws

--- src/Cide/Docker.hs
+++ src/Cide/Docker.hs
@@ -6,7 +6,7 @@ import Text.Regexp (mkRegex, matchRegex)
 
 data DockerEnv =
   DockerEnv {
-  env :: [(String, String)]
+    env :: [(String, String)]
   } deriving (Show)
 
 withDocker :: IO (DockerEnv)
@@ -19,22 +19,22 @@ withDocker = checkDocker
 checkDocker nextStep = do
   ec <- system "which docker >/dev/null"
   case ec of
-  ExitSuccess ->
-    DockerEnv []
-  ExitFailure _ ->
-    error "cide requires docker. Install from http://docker.com"
+    ExitSuccess ->
+      DockerEnv []
+    ExitFailure _ ->
+      error "cide requires docker. Install from http://docker.com"
 
 envRegex = mkRegex "export (\w+)=(.*)"
 
 setupWithBoot2docker nextStep = do
   dockerHost <- getEnv("DOCKER_HOST")
   if dockerHost == ""
-  ec <- system "which boot2docker >/dev/null"
-  case ec of
-    ExitSuccess ->
-      setupDocker
-    ExitFailure ->
-      error "cide requires boot2docker on OSX to work. Install from http://boot2docker.io"
+    ec <- system "which boot2docker >/dev/null"
+    case ec of
+      ExitSuccess ->
+        setupDocker
+      ExitFailure ->
+        error "cide requires boot2docker on OSX to work. Install from http://boot2docker.io"
   where
   setupDocker = do
     envStr <- readProcess "boot2docker" ["shellinit"] []
