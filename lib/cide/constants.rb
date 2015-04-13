490e68fb46399db32f5d60c954edadffd5dd57c9
zimbatm <zimbatm@zimbatm.com>
2015-04-13 10:35:45 +0100

    .

--- lib/cide/constants.rb
+++ lib/cide/constants.rb
@@ -6,6 +6,6 @@ module CIDE
   SSH_CONFIG_PATH = File.join(DIR, 'cide', SSH_CONFIG_FILE)
   CONFIG_FILE = '.cide.yml'
   CIDE_DIR = '/cide'
-  CIDE_SRC_DIR = File.join(CIDE_DIR, '/src')
-  CIDE_SSH_DIR = File.join(CIDE_DIR, '/.ssh')
+  CIDE_SRC_DIR = File.join(CIDE_DIR, 'src')
+  CIDE_SSH_DIR = File.join(CIDE_DIR, '.ssh')
 end
