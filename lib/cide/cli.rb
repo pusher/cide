4b83bf772ba3e6d0bd6df9662d13753c857e0ae1
zimbatm <zimbatm@zimbatm.com>
2016-02-12 12:49:24 +0000

    New command: cide package
    This command is used to build an archive of the project

--- lib/cide/cli.rb
+++ lib/cide/cli.rb
@@ -21,7 +21,7 @@ module CIDE
     desc 'exec', 'Builds an image and executes the run script'
 
     method_option 'name',
-      desc: 'Name of the build',
+      desc: 'Name of the image',
       aliases: %w(-n -t),
       default: File.basename(Dir.pwd)
 
@@ -59,7 +59,7 @@ module CIDE
     def exec
       setup_docker
 
-      tag = "cide/#{CIDE::Docker.id options.name}"
+      tag = name_to_tag options.name
 
       banner 'Config'
       config = ConfigFile.load(Dir.pwd)
@@ -103,6 +103,116 @@ module CIDE
       runner.cleanup! if runner
     end
 
+    desc 'package', 'Builds a package from the container script/build'
+    method_option 'name',
+      desc: 'Name of the image',
+      aliases: %w(-n -t),
+      default: File.basename(Dir.pwd)
+
+    method_option 'ssh_key',
+      desc: 'Path to a ssh key to import into the docker image',
+      aliases: ['-s'],
+      default: '~/.ssh/id_rsa'
+
+    method_option 'package',
+      desc: 'Name of the package',
+      default: File.basename(Dir.pwd)
+
+    method_option 'upload',
+      desc: 'Whenever to upload the result to S3',
+      type: :boolean,
+      default: true
+
+    method_option 'set-version',
+      desc: 'Tells cide the package version, otherwise extracted from git',
+      default: nil
+
+    # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_REGION need to be passed
+    # either trough the env or ~/.aws/credentials file
+    method_option 'aws_bucket',
+      desc: 'AWS_BUCKET',
+      default: ENV['AWS_BUCKET']
+
+    def package
+      fail 'missing AWS_BUCKET' if options.upload && !options.aws_bucket
+
+      tag = name_to_tag options.name
+
+      build_root = File.expand_path('.package')
+      guest_export_dir = '/cide/package'
+      host_export_dir  = File.join(build_root, 'package')
+
+      version = options.version || (
+        git_branch = `git symbolic-ref --short -q HEAD || echo unknown`.strip
+        git_rev = `git rev-parse --short HEAD`.strip
+        "#{git_branch}-#{git_rev}"
+      )
+
+      timestamp = Time.now.strftime('%Y-%m-%d_%H%M%S')
+      tar_name = "#{options.package}.#{timestamp}.#{version}.tar.gz"
+      tar_path = File.join(build_root, tar_name)
+
+      banner 'Config'
+      config = ConfigFile.load(Dir.pwd)
+      say_status :config, config.inspect
+
+      ## Build ##
+      banner 'Build'
+      builder = Builder.new(config)
+      builder.build(
+        pull: options.pull,
+        ssh_key: File.expand_path(options.ssh_key),
+        tag: tag,
+      )
+
+      ## Run ##
+      banner 'Run'
+      runner = Runner.new(
+        command: ['script/build', guest_export_dir],
+        env: config.env,
+        links: config.links,
+        tag: tag,
+      )
+      runner.run!
+
+      ## Export ##
+      banner 'Export'
+
+      FileUtils.rm_rf(build_root)
+
+      runner.export!(
+        guest_dir: guest_export_dir,
+        host_dir: host_export_dir,
+      )
+
+      # Create archive
+      puts "Package: #{tar_name}"
+      system('tar', '-czf', tar_path, '-C', host_export_dir, '.')
+
+      ## Upload ##
+
+      return unless options.upload
+      banner 'Upload'
+
+      require 'aws-sdk'
+      s3 = Aws::S3::Client.new
+      resp = s3.put_object(
+        bucket: options.aws_bucket,
+        key: tar_name,
+        body: File.open(tar_path),
+      )
+      p resp
+    rescue Docker::Error => ex
+      exit ex.exitstatus
+    rescue RuntimeError => ex
+      $stderr.puts ex.to_s
+      exit 1
+    ensure
+      FileUtils.rm_rf(build_root) if options.upload
+
+      runner.cleanup! if runner
+    end
+
     desc 'debug', 'Opens a debug console in the last project image'
     method_option 'name',
       desc: 'Name of the build',
@@ -114,7 +224,7 @@ module CIDE
     def debug
       setup_docker
 
-      tag = "cide/#{CIDE::Docker.id options.name}"
+      tag = name_to_tag options.name
 
       ## Config ##
       banner 'Config'
@@ -200,9 +310,15 @@ module CIDE
 
     private
 
-    LINE_SIZE = 78.0
+    # Prefixes the tag to make it recognizable by the cleaner
+    # Makes sure it's a valid tag
+    def name_to_tag(name)
+      "cide/#{CIDE::Docker.id name}"
+    end
+
+    LINE_WIDTH = 78.0
     def banner(text)
-      pad = (LINE_SIZE - text.size - 4) / 2
+      pad = (LINE_WIDTH - text.size - 4) / 2
       pad = 0 if pad < 0
       puts '=' * pad.floor + "[ #{text} ]" + '=' * pad.ceil
     end
